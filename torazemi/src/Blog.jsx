30ee1991ec2a8644251b56c012f6d8c443d67eaa
ken505 <68226398+ken505@users.noreply.github.com>
2021-01-30 15:46:15 +0900

    wip:

--- torazemi/src/Blog.jsx
+++ torazemi/src/Blog.jsx
@@ -6,23 +6,36 @@ class Blog extends React.Component {
     super(props);
     this.state = {
       isPublished: false,
-      order: 1
+      count: 0,
     }
   }
 
+  // ボタンがクリックされたら、いいねをカウントアップする。
+  componentDidMount() {
+    document.getElementById('counter').addEventListener('click', this.countUp)
+  }
+
   // 公開状態を反転する関数。
-togglePublished = () => {
-  this.setState( {
-    isPublished: !this.state.isPublished
-  })
-};
+  togglePublished = () => {
+    this.setState({
+      isPublished: !this.state.isPublished,
+    })
+  };
+
+  countUp = () => {
+    this.setState(state => {
+      return {count: this.state.count + 1 }
+    })
+  }
 
   render() {
     return (
       <>
         <Article
           title={"GitHub でローカルファイルを管理"}
-          isPublished={this.state.isPublished} toggle={() => this.togglePublished()}
+          isPublished={this.state.isPublished}
+          toggle={() => this.togglePublished()}
+          count={this.state.count}
         />
       </>
     );
