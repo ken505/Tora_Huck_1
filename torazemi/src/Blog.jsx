5c2d6319dcdfd565e7b23ae2cf1df2cbf53a1dc0
ken505 <68226398+ken505@users.noreply.github.com>
2021-01-31 08:24:43 +0900

    wip:

--- torazemi/src/Blog.jsx
+++ torazemi/src/Blog.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Article from "./Article";
+import * as FooBar from './components/FooBar';
+import Hoge from './components/Hoge'
 
 class Blog extends React.Component {
   constructor(props) {
@@ -19,6 +21,10 @@ class Blog extends React.Component {
     if (this.state.count >= 10) {
       this.setState({ count: 0 });
     }
+  }
+
+  componentWillUnmount() {
+    document.getElementById("counter").removeEventListener("click", this.countUp);
   }
 
   // 公開状態を反転する関数。
@@ -43,6 +49,9 @@ class Blog extends React.Component {
           toggle={() => this.togglePublished()}
           count={this.state.count}
         />
+        <FooBar.Foo />
+        <FooBar.Bar />
+        <Hoge />
       </>
     );
   }
