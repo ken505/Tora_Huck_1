99ec4c2959eecc55ca931b26f502a26d151e0175
ken505 <68226398+ken505@users.noreply.github.com>
2021-01-30 14:10:38 +0900

    wip:

--- torazemi/src/Blog.jsx
+++ torazemi/src/Blog.jsx
@@ -4,18 +4,27 @@ import Article from "./Article";
 class Blog extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isPublished: false,
+      order: 1
+    }
   }
+
+  // 公開状態を反転する関数。
+togglePublished = () => {
+  this.setState( {
+    isPublished: !this.state.isPublished
+  })
+};
+
   render() {
-    const authorName = "ToraHuck";
     return (
-      <div>
+      <>
         <Article
-          title={"React の使い方"}
-          order={2021}
-          isPublished={true}
-          author={authorName}
+          title={"GitHub でローカルファイルを管理"}
+          isPublished={this.state.isPublished} toggle={() => this.togglePublished()}
         />
-      </div>
+      </>
     );
   }
 }
