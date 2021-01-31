269ed14c62367947c0eb8d359c62aa3eae8fb40a
ken505 <68226398+ken505@users.noreply.github.com>
2021-01-31 09:43:39 +0900

    wip:

--- torazemi/src/Article.jsx
+++ torazemi/src/Article.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 import LikeButton from "./LikeButton";
 
 const Article = (props) => {
+  const [isPublished, togglePublished] = useState(false);
   return (
     <div>
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態:</label>
       <input
         type="checkbox"
-        checked={props.isPublished}
+        checked={isPublished}
         id="check"
-        onClick={() => props.toggle()}
+        onClick={() => togglePublished(!isPublished)}
       />
       <LikeButton count={props.count} />
     </div>
-  )
+  );
 };
 
 export default Article;
--- torazemi/src/Blog.jsx
+++ torazemi/src/Blog.jsx
@@ -1,60 +1,40 @@
 import React from "react";
 import Article from "./Article";
-import * as FooBar from './components/FooBar';
-import Hoge from './components/Hoge'
+import * as FooBar from "./components/FooBar";
+import Hoge from "./components/Hoge";
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0,
-    };
-  }
+const Blog = () => {
+  // componentDidMount() {
+  //   document.getElementById("counter").addEventListener("click", this.countUp);
+  // }
 
-  // ボタンがクリックされたら、いいねをカウントアップする。
-  componentDidMount() {
-    document.getElementById("counter").addEventListener("click", this.countUp);
-  }
+  // componentDidUpdate() {
+  //   if (this.state.count >= 10) {
+  //     this.setState({ count: 0 });
+  //   }
+  // }
 
-  componentDidUpdate() {
-    if (this.state.count >= 10) {
-      this.setState({ count: 0 });
-    }
-  }
+  // componentWillUnmount() {
+  //   document.getElementById("counter").removeEventListener("click", this.countUp);
+  // }
 
-  componentWillUnmount() {
-    document.getElementById("counter").removeEventListener("click", this.countUp);
-  }
+  // countUp = () => {
+  //   this.setState((state) => {
+  //     return { count: this.state.count + 1 };
+  //   });
+  // };
 
-  // 公開状態を反転する関数。
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished,
-    });
-  };
-
-  countUp = () => {
-    this.setState((state) => {
-      return { count: this.state.count + 1 };
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <Article
-          title={"GitHub でローカルファイルを管理"}
-          isPublished={this.state.isPublished}
-          toggle={() => this.togglePublished()}
-          count={this.state.count}
-        />
-        <FooBar.Foo />
-        <FooBar.Bar />
-        <Hoge />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Article
+        title={"GitHub でローカルファイルを管理"}
+        // count={this.state.count}
+      />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </>
+  );
+};
 
 export default Blog;
